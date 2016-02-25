779e1ddb87a808b529aba4723350ecd503b6a557
Amayadream <524806599@qq.com>
2016-02-25 22:33:30 +0800

    update

--- src/main/java/com/amayadream/webchat/filter/LoginFilter.java
+++ src/main/java/com/amayadream/webchat/filter/LoginFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * NAME   :  EmailSystem/com.amayadream.filter
